5e5127b093aa760cf5c3c1c49ea932f8b1af1ae3
jbencook <jbenjamincook@gmail.com>
2016-08-25 15:07:35 -0500

    ax opencv_legacy

--- setup.py
+++ setup.py
@@ -9,28 +9,18 @@ source = [
     "cpptrack/CompressiveTrackerModule.cpp",
     "cpptrack/BidirectionTrackerModule.cpp",
     "cpptrack/RandomFullTracker.cpp",
-    # TLD
-    "cpptrack/TldTrackerModule.cpp",
-    "cpptrack/TldTracker.cpp",
-    "cpptrack/TLD.cpp",
-    "cpptrack/tld_utils.cpp",
-    "cpptrack/LKTracker.cpp",
-    "cpptrack/FerNNClassifier.cpp",
-    # BiTLD
-    "cpptrack/BiTldTrackerModule.cpp",
 ]
 
 extensions = Extension(
     "cpptrack",
     sources=source,
-    libraries=["opencv_highgui", "opencv_core", "opencv_imgproc", "opencv_legacy"],
+    libraries=["opencv_highgui", "opencv_core", "opencv_imgproc"],
     language="c++",
 )
 
 setup(
-    name = "tracking",
-    author = "John Doherty, Charlie Ma",
-    packages = ["tracking", "pytrack"],
-    ext_modules = cythonize(extensions),
-    data_files=[('config', ['./cpptrack/TLDparameters.yml'])]
+    name="tracking",
+    author="John Doherty, Charlie Ma",
+    packages=["tracking", "pytrack"],
+    ext_modules=cythonize(extensions),
 )
